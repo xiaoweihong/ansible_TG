32e5b1bf342fbb62d668100d3c1f1db4afcf00c2
xiaoweihong <403828237@qq.com>
2019-07-18 09:47:41 +0800

    细节优化
    1. loki离线视频配置优化
    2. 新增修改ip配置

--- install.sh
+++ install.sh
@@ -4,13 +4,15 @@ function checkAnsible(){
   ansible --version >/dev/null 2>&1
   if [ $? -ne 0 ];then
     echo "ansible not installed,begin install ansible" 
-    tar zcvf roles/common/files/package/software_package.tgz -C /opt/
+    tar zxvf roles/common/files/package/software_package.tgz -C /opt/
     cp roles/common/files/conf/apt/sources.list_local /etc/apt/sources.list
     cp roles/common/files/conf/apt/02allow-unsigned /etc/apt/apt.conf.d/
     apt-get update
-    apt-get install ansible
+    apt-get -y install ansible
     echo "install completed"
-  fi
+ else
+    echo "ansible has been installed"
+ fi
 
 }
 
