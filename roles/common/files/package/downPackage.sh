6a7958c277ad614ea733dc0e5f85bc15a9b2f406
xiaoweihong <403828237@qq.com>
2019-07-19 10:59:40 +0800

    优化部署方式
    1. 变更平台包的部署方式，变更为集成部署，不在单独部署fse,map,Mutischeduler
    2. 修改ftp用户的存放目录，由/data/ftp变更为/platformData/ftp

--- roles/common/files/package/downPackage.sh
+++ roles/common/files/package/downPackage.sh
@@ -7,9 +7,9 @@ function downloadPackage(){
    wget ${DOWNLOAD_URL}/bigtoe_DeepEngine-4.0.1-all.tar.gz
    wget ${DOWNLOAD_URL}/software_package.tgz
    wget ${DOWNLOAD_URL}/platformTG.tgz
-   wget ${DOWNLOAD_URL}/mutischeduler-1.0.0.tar.gz
-   wget ${DOWNLOAD_URL}/fse-3.5.1.tar.gz
-   wget ${DOWNLOAD_URL}/map.tgz
+ #  wget ${DOWNLOAD_URL}/mutischeduler-1.0.0.tar.gz
+ #  wget ${DOWNLOAD_URL}/fse-3.5.1.tar.gz
+ #  wget ${DOWNLOAD_URL}/map.tgz
    wget ${DOWNLOAD_URL}/update_ssh_for_1604_20190715.tgz
    wget ${DOWNLOAD_URL}/importer-1.3.6.img
    wget ${DOWNLOAD_URL}/link-0.19.0-job-hostnetwork.img
