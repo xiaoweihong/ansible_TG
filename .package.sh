7f0dd275bedc1740778e385fa575008f3669dfef
xiaoweihong <40382837@qq.com>
2019-07-23 10:09:16 +0800

    打包相关优化
    1. 适配配置文件的修改

--- .package.sh
+++ .package.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 PUBLISH_DATE=$(date +%Y%m%d)
-PUBLISH_PATH=/root/platformTG_${PUBLISH_DATE}
+PUBLISH_PATH=/backup/xiaowei/project/release/platformTG/release/platform_${PUBLISH_DATE}
+
 
 echo "clean package"
 cd roles/common/files/package
 rm -f *gz
 rm -f *.img
 
-mkdir ${PUBLISH_PATH} -p && cd ${PUBLISH_PATH}
+if [  -d ${PUBLISH_PATH} ];then
+   rm  -rf ${PUBLISH_PATH}
+fi
+
+mkdir ${PUBLISH_PATH} -p
+
+cd ${PUBLISH_PATH}
 
 cp -pvr /etc/ansible/ ${PUBLISH_PATH}
 
@@ -16,4 +23,7 @@ ln -s ansible/roles/common/files/package/software_package.tgz software_package.t
 ln -s ansible/install.sh install.sh
 
 cd ansible/roles/common/files/package/ && bash downPackage.sh
-cd -
+cd ${PUBLISH_PATH}
+
+#cd ../
+#tar zcvf platform_${PUBLISH_DATE}.tgz platform_${PUBLISH_DATE}
