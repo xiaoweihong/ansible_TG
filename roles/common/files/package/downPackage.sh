ca44777b370dcfaf027a0e1e345f60b36dc79c78
xiaoweihong <40382837@qq.com>
2019-07-30 18:40:00 +0800

    优化配置
    1. 优化部署脚本
    2. 新增打包脚本
    3. 更新应用的配置文件

--- roles/common/files/package/downPackage.sh
+++ roles/common/files/package/downPackage.sh
@@ -11,8 +11,10 @@ function downloadPackage(){
  #  wget ${DOWNLOAD_URL}/fse-3.5.1.tar.gz
  #  wget ${DOWNLOAD_URL}/map.tgz
    wget ${DOWNLOAD_URL}/update_ssh_for_1604_20190715.tgz
-   wget ${DOWNLOAD_URL}/importer-1.3.6.img
-   wget ${DOWNLOAD_URL}/link-0.19.0-job-hostnetwork.img
+   wget ${DOWNLOAD_URL}/importer-1.3.6.tar
+   wget ${DOWNLOAD_URL}/link-0.19.0-job-hostnetwork.tar
+   wget ${DOWNLOAD_URL}/hm_importer-0.12.5.tar
+   wget ${DOWNLOAD_URL}/hw_importer-0.8.0.tar
 
 }
 downloadPackage
