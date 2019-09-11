a4f03f0648f4957b285df6d94904878830e8af36
xiaoweihong <40382837@qq.com>
2019-09-11 14:43:07 +0800

    修正第二次安装ansible目录存在的bug

--- install.sh
+++ install.sh
@@ -102,6 +102,12 @@ echo "请选择:"
 }
 
 function singleDeploy(){
+
+  if [ -d /etc/ansible ];then
+    rm -f /etc/ansible
+  else
+    ln -s ${SHELL_DIR}/ansible /etc/ansible
+  fi
 . ansible/scripts/config_arcee.sh
 
   echo "[master]
