585d2a7fbe71a32244090d91c5e73a2c2177f729
xiaoweihong <40382837@qq.com>
2019-10-25 14:49:16 +0800

    loki分区表sql

---
+++ loki_partation.sql
@@ -0,0 +1,11 @@
+select AutoCreatePartitionTable('faces', 'face_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('vehicle_capture_index', 'vehicle_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('vehicle_capture', 'vehicle_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('nonmotor_capture', 'nonmotor_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('pedestrian_capture', 'pedestrian_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('faces_index', 'face_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
