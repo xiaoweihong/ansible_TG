0049da59c2d4a1c5cb04d630745db660755df8fb
xiaoweihong <40382837@qq.com>
2019-11-06 10:06:38 +0800

    loki分表语句

--- loki_partation.sql
+++ loki_partation.sql
@@ -1,3 +1,9 @@
+su - postgres
+
+psql
+
+\c deepface_v5
+
 select AutoCreatePartitionTable('faces', 'face_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
 
 select AutoCreatePartitionTable('vehicle_capture_index', 'vehicle_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
@@ -9,3 +15,8 @@ select AutoCreatePartitionTable('nonmotor_capture', 'nonmotor_id', thisWeekName(
 select AutoCreatePartitionTable('pedestrian_capture', 'pedestrian_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
 
 select AutoCreatePartitionTable('faces_index', 'face_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('white-warn', 'face_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
+select AutoCreatePartitionTable('white-warn_index', 'face_id', thisWeekName(), timeZone(weekBegin(0)), timeZone(weekBegin(1)));
+
