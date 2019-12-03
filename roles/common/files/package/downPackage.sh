b6c813db225c0de01b317a045d1c45b6f6eee026
xiaoweihong <40382837@qq.com>
2019-12-03 17:19:11 +0800

    暂存

--- roles/common/files/package/downPackage.sh
+++ roles/common/files/package/downPackage.sh
@@ -4,7 +4,7 @@ DOWNLOAD_URL="192.168.2.189:8050/platformSoftware"
 
 function downloadPackage(){
 
-   wget ${DOWNLOAD_URL}/bigtoe_DeepEngine-4.0.1-all.tar.gz
+#   wget ${DOWNLOAD_URL}/bigtoe_DeepEngine-4.0.1-all.tar.gz
    wget ${DOWNLOAD_URL}/software_package.tgz
    wget ${DOWNLOAD_URL}/platformTG.tgz
  #  wget ${DOWNLOAD_URL}/mutischeduler-1.0.0.tar.gz
