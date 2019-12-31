a2d23df0d109a48a47312a4095c770b01620f5f2
xiaoweihong <403828237@qq.com>
2019-12-31 17:59:32 +0800

    1231版本中间状态(未完成)

---
+++ roles/TG/templates/nginx/start_nginx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NAME="nginx"
+CID=$(docker ps | awk '{if($NF=="'${NAME}'")print $1}')
+if [[ $CID"x" != "x" ]];then
+    docker rm --force $CID
+fi
+
+docker run -d --restart always \
+--network host \
+-v /var/www:/var/www \
+-v /home/ubuntu/platformTG/jormougand/latest:/home/ubuntu/platformTG/jormougand/latest \
+-v /etc/nginx/conf.d:/etc/nginx/conf.d \
+-v /etc/nginx/nginx.conf:/etc/nginx/nginx.conf \
+-v /platformData:/platformData \
+-v /data/bigtoe:/data/bigtoe \
+-v /etc/localtime:/etc/localtime \
+--name $NAME \
+nginx:1.17.3
--- roles/common/files/package/downPackage.sh
+++ roles/common/files/package/downPackage.sh
@@ -6,19 +6,16 @@ function downloadPackage(){
 
 #   wget ${DOWNLOAD_URL}/bigtoe_DeepCloud-10.1.1-all.tar.gz
    wget ${DOWNLOAD_URL}/software_package.tgz
-#   wget ${DOWNLOAD_URL}/platformTG.tgz
- #  wget ${DOWNLOAD_URL}/mutischeduler-1.0.0.tar.gz
- #  wget ${DOWNLOAD_URL}/fse-3.5.1.tar.gz
- #  wget ${DOWNLOAD_URL}/map.tgz
+   wget ${DOWNLOAD_URL}/platformTG.tgz
+   wget ${DOWNLOAD_URL}/mutischeduler-1.0.0.tar.gz
    wget ${DOWNLOAD_URL}/update_ssh_for_1604_20191016.tgz
-#   wget ${DOWNLOAD_URL}/importer-1.3.6.tar
-#   wget ${DOWNLOAD_URL}/link-0.21.0.tar
-#   wget ${DOWNLOAD_URL}/hm_importer-0.12.5.tar
-#   wget ${DOWNLOAD_URL}/hw_importer-0.8.0.tar
-#   wget ${DOWNLOAD_URL}/dc84_sdk-0.1.0.tar
-#   wget ${DOWNLOAD_URL}/vse-3.7.1-p2.tar.gz
-#   wget ${DOWNLOAD_URL}/tools.zip
-#   wget ${DOWNLOAD_URL}/tiles.tar.gz
+   wget ${DOWNLOAD_URL}/importer-1.3.6.tar
+   wget ${DOWNLOAD_URL}/hm_importer-0.12.5.tar
+   wget ${DOWNLOAD_URL}/hw_importer-0.8.0.tar
+   wget ${DOWNLOAD_URL}/dc84_sdk-0.1.0.tar
+   wget ${DOWNLOAD_URL}/tools.zip
+   wget ${DOWNLOAD_URL}/godeye_20191219_deviceid.tar
+   wget ${DOWNLOAD_URL}/geojson.tar.gz
 
 }
 downloadPackage
